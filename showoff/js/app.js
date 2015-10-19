458d459c9d428c637c649367cccf3847455952d1
intheon <allobon@gmail.com>
2015-10-19 21:33:09 +0100

    got somewhere!

--- showoff/js/app.js
+++ showoff/js/app.js
@@ -3,11 +3,35 @@ $(document).ready(function(){
 
 	$.scrollify({
 		section: 		".content-area",
-		sectionName : "name",
-		easing: "easeOutExpo",
-		scrollSpeed: 1000,
-		offset : 0,
-		scrollbars: false
+		sectionName: 	"name",
+		easing: 		"easeOutExpo",
+		scrollSpeed: 	1000,
+		offset : 		0,
+		scrollbars: 	false,
+		before: 		function(event){
+			// event is just the number of the panel
+			var integer = event;
+			$(".nav-link").removeClass("active-link");
+
+			switch (integer)
+			{
+				case 0:
+					$("#home-link").addClass("active-link");
+				break;
+				case 1:
+					$("#portfolio-link").addClass("active-link");
+				break;
+				case 2:
+					$("#about-link").addClass("active-link");
+				break;
+				case 3:
+					$("#contact-link").addClass("active-link");
+				break;
+				default:
+					console.log("hardflips are hard");
+				break;
+			}
+		}
 	});
 
 	$(".nav-link").click(function(event){
