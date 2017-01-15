41f7ef2e1d476ed8350c3a4baeab5a18a6c5e616
Ben <synthy@protonmail.com>
2017-01-15 00:07:42 +0000

    tidy

--- server.js
+++ server.js
@@ -21,9 +21,6 @@ app.use(methodOverride());
 
 
 
-// Expose the jspm packages + config as well as the client front-end
-app.use('/jspm_packages',  express.static(__dirname + '/jspm_packages'));
-app.use("/config.js", express.static(__dirname + "/config.js"));
 app.use(express.static(__dirname + "/client"));
 
 
