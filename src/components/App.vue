dbab59c28558cea286b34bc8de2a80edb8cd2fb1
Ben S <allobon@gmail.com>
2017-03-06 14:11:03 +0000

    triangles!

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,40 +1,107 @@
 <template>
 	<div>
 
-    <div class="background-triangle"></div>
+    <div class="background-triangle-left"></div>
+    <div class="background-triangle-right"></div>
+
 
     <div class="content-area-wrapper">
       <div class="big-triangle-wrapping-mask"></div>
 
+
       <section class="content-area first">
-        <p>Live-edge sed williamsburg kombucha venmo food truck authentic, wolf DIY. Vape consequat pariatur placeat. Eu assumenda id, vape mixtape semiotics 90's voluptate consequat retro. Organic pork belly four dollar toast waistcoat bespoke 3 wolf moon, cardigan labore hexagon. Et id lyft, meggings iceland scenester vaporware snackwave sustainable succulents iPhone odio. Messenger bag irure quis yuccie, twee paleo duis blog in. Cray waistcoat placeat yr, art party keffiyeh ut live-edge.</p>
-        <p>Vero hashtag mumblecore dolore keffiyeh venmo. Culpa nihil listicle keffiyeh. Quis cillum sed, pitchfork normcore sapiente yr fixie bitters raw denim kale chips four dollar toast culpa. Jianbing mustache irure, quis ad synth roof party man braid non raw denim et banjo pork belly hexagon bushwick. Fashion axe art party poutine thundercats assumenda, humblebrag semiotics vinyl. Affogato poke sustainable deep v sriracha lyft, mixtape laborum truffaut keffiyeh. Poutine fanny pack mixtape, activated charcoal sint austin edison bulb squid disrupt bicycle rights culpa artisan godard hot chicken letterpress.</p>
-        <p>Microdosing narwhal austin fixie do, food truck cardigan nulla farm-to-table actually gochujang. Fanny pack coloring book delectus fap. Umami nesciunt chicharrones proident normcore whatever. Helvetica labore vegan, glossier fanny pack eiusmod keffiyeh copper mug mollit polaroid officia portland nesciunt ad. Narwhal do four dollar toast, XOXO mumblecore laboris ramps. Locavore et lyft laborum, master cleanse tbh nihil irony. Dolor kinfolk mixtape, lomo prism cray elit listicle fingerstache poke.</p>
-        <p>Kogi assumenda excepteur, live-edge echo park try-hard keytar sriracha in reprehenderit consectetur post-ironic. Kombucha everyday carry whatever butcher bushwick, banjo readymade migas. Kitsch paleo pug, heirloom neutra narwhal delectus viral vice twee lo-fi nostrud raclette butcher. Proident vinyl woke tousled, actually laboris semiotics irure artisan viral hoodie green juice celiac magna. Lumbersexual succulents pickled hashtag mustache. Veniam seitan gastropub, bushwick hexagon hell of vice normcore franzen gluten-free 3 wolf moon slow-carb. Organic lo-fi art party, selvage typewriter et cardigan shabby chic.</p>
+        <h1>Howdy!</h1>
+        <p>I'm Ben, developer of internets.</p>
+        <p>I focus on building web-based apps, mostly centered around dashboards and tools.</p>
+        <p>Building cross-platform is one of my strengths, as i do that in both my day job and for projects.</p>
+        <p>Take a look at my portfolio to see some of my work, or drop me a line and I'll get right back to you!</p>
+      </section>
+
+      <section class="content-area">
+        <h1>Portfolio</h1>
+
+        <section class="portfolio-item">
+
+          <div class="portfolio-wrapping skate"></div>
+
+          <div class="portfolio-content">
+            <h2>Skate Locate</h2>
+            <p>A pretty modern website as far as the technology is concerned.</p>
+            <p>Add, view, and filter skateparks in your area of interest</p>
+            <p>Built with the MEAN stack, bundled with JSPM, Cloudinary for images, Leaflet/Mapbox for the map engine.</p>
+            <ul>
+              <li>100% Javascript on front and back end.</li>
+              <li>MongoDB for data, and Angular.js for the databinding and keeping the UI snappy.</li>
+              <li>Add skateparks, filter by tag or string, see most recently added and voted.</li>
+              <li>Deployed to Heroku, and live for all!</li>
+            </ul>
+          </div>
+
+        </section>
+
+        <section class="portfolio-item">
+          <h2>Ed Hall Audio</h2>
+          <p>Created this for a buddy of mine who runs his own recording studio and mixing service.</p>
+          <p>Ed works crazy hard so if you're a musician who needs some recording help give him a shout!</p>
+          <ul>
+            <li>Responsive Design</li>
+            <li>Instagram and Soundcloud feeds</li>
+            <li>Contact form</li>
+            <li>Lightweight, uses only jQuery and Semantic UI</li>
+          </ul>
+        </section>
+
+        <section class="portfolio-item">
+          <h2>Springboard</h2>
+          <p>Google used to offer a personalised dashboard called iGoogle but discontinued it, so I made my own.</p>
+          <p>Most complex piece of software I've written from scratch, allows you to have persistent modules that each have their own unique state. You can view your emails, todo's, keep track of your spending etc!</p>
+          <ul>
+            <li>REST API Backend to interact with database</li>
+            <li>Multi-user system with states for each widget</li>
+            <li>Heavy use of JavaScript and PHP for some wizardry</li>
+            <li>Semantic UI to make it look pretty</li>
+          </ul>
+        </section>
+
+
+        <section class="portfolio-item">
+          <h2>Semantic Twitter</h2>
+          <p>An example of what happens when you mash up web API's</p>
+          <p>Asks the user for a string, and uses the Twitter API to retreive that from their DB, then crams it through 'Semantic API' to tell you what the users feeling is.</p>
+          <ul>
+            <li>Ajax based system which uses the Twitter API, and Semantic UI</li>
+            <li>Displays users results as soon as they have been parsed</li>
+          </ul>
+        </section>
+
+        <section class="portfolio-item">
+          <h2>Grammatical Numbers</h2>
+          <p>A pretty modern website as far as the technology is concerned.</p>
+          <p>Add, view, and filter skateparks in your area of interest</p>
+          <ul>
+            <li>Ajax based system which uses the Twitter API, and Semantic UI</li>
+            <li>Displays users results as soon as they have been parsed</li>
+          </ul>
+        </section>
+
+        <section class="portfolio-item">
+          <h2>Promo Codes Generator</h2>
+          <p>I needed to generate promo codes for a client so they could distribute out free log in's for their magazine.</p>
+          <p>Rather than user excel, i made this service as I no doubt will need it again</p>
+          <ul>
+            <li>Random code generator using chance.js</li>
+            <li>Allows selection of quantity of codes</li>
+            <li>Some wizardry to allow the user to download to a csv</li>
+          </ul>
+        </section>
+
       </section>
       <section class="content-area">
-        <p>Live-edge sed williamsburg kombucha venmo food truck authentic, wolf DIY. Vape consequat pariatur placeat. Eu assumenda id, vape mixtape semiotics 90's voluptate consequat retro. Organic pork belly four dollar toast waistcoat bespoke 3 wolf moon, cardigan labore hexagon. Et id lyft, meggings iceland scenester vaporware snackwave sustainable succulents iPhone odio. Messenger bag irure quis yuccie, twee paleo duis blog in. Cray waistcoat placeat yr, art party keffiyeh ut live-edge.</p>
-        <p>Vero hashtag mumblecore dolore keffiyeh venmo. Culpa nihil listicle keffiyeh. Quis cillum sed, pitchfork normcore sapiente yr fixie bitters raw denim kale chips four dollar toast culpa. Jianbing mustache irure, quis ad synth roof party man braid non raw denim et banjo pork belly hexagon bushwick. Fashion axe art party poutine thundercats assumenda, humblebrag semiotics vinyl. Affogato poke sustainable deep v sriracha lyft, mixtape laborum truffaut keffiyeh. Poutine fanny pack mixtape, activated charcoal sint austin edison bulb squid disrupt bicycle rights culpa artisan godard hot chicken letterpress.</p>
-        <p>Microdosing narwhal austin fixie do, food truck cardigan nulla farm-to-table actually gochujang. Fanny pack coloring book delectus fap. Umami nesciunt chicharrones proident normcore whatever. Helvetica labore vegan, glossier fanny pack eiusmod keffiyeh copper mug mollit polaroid officia portland nesciunt ad. Narwhal do four dollar toast, XOXO mumblecore laboris ramps. Locavore et lyft laborum, master cleanse tbh nihil irony. Dolor kinfolk mixtape, lomo prism cray elit listicle fingerstache poke.</p>
-        <p>Kogi assumenda excepteur, live-edge echo park try-hard keytar sriracha in reprehenderit consectetur post-ironic. Kombucha everyday carry whatever butcher bushwick, banjo readymade migas. Kitsch paleo pug, heirloom neutra narwhal delectus viral vice twee lo-fi nostrud raclette butcher. Proident vinyl woke tousled, actually laboris semiotics irure artisan viral hoodie green juice celiac magna. Lumbersexual succulents pickled hashtag mustache. Veniam seitan gastropub, bushwick hexagon hell of vice normcore franzen gluten-free 3 wolf moon slow-carb. Organic lo-fi art party, selvage typewriter et cardigan shabby chic.</p>
-      </section>
-      <section class="content-area">
-        <p>Live-edge sed williamsburg kombucha venmo food truck authentic, wolf DIY. Vape consequat pariatur placeat. Eu assumenda id, vape mixtape semiotics 90's voluptate consequat retro. Organic pork belly four dollar toast waistcoat bespoke 3 wolf moon, cardigan labore hexagon. Et id lyft, meggings iceland scenester vaporware snackwave sustainable succulents iPhone odio. Messenger bag irure quis yuccie, twee paleo duis blog in. Cray waistcoat placeat yr, art party keffiyeh ut live-edge.</p>
-        <p>Vero hashtag mumblecore dolore keffiyeh venmo. Culpa nihil listicle keffiyeh. Quis cillum sed, pitchfork normcore sapiente yr fixie bitters raw denim kale chips four dollar toast culpa. Jianbing mustache irure, quis ad synth roof party man braid non raw denim et banjo pork belly hexagon bushwick. Fashion axe art party poutine thundercats assumenda, humblebrag semiotics vinyl. Affogato poke sustainable deep v sriracha lyft, mixtape laborum truffaut keffiyeh. Poutine fanny pack mixtape, activated charcoal sint austin edison bulb squid disrupt bicycle rights culpa artisan godard hot chicken letterpress.</p>
-        <p>Microdosing narwhal austin fixie do, food truck cardigan nulla farm-to-table actually gochujang. Fanny pack coloring book delectus fap. Umami nesciunt chicharrones proident normcore whatever. Helvetica labore vegan, glossier fanny pack eiusmod keffiyeh copper mug mollit polaroid officia portland nesciunt ad. Narwhal do four dollar toast, XOXO mumblecore laboris ramps. Locavore et lyft laborum, master cleanse tbh nihil irony. Dolor kinfolk mixtape, lomo prism cray elit listicle fingerstache poke.</p>
-        <p>Kogi assumenda excepteur, live-edge echo park try-hard keytar sriracha in reprehenderit consectetur post-ironic. Kombucha everyday carry whatever butcher bushwick, banjo readymade migas. Kitsch paleo pug, heirloom neutra narwhal delectus viral vice twee lo-fi nostrud raclette butcher. Proident vinyl woke tousled, actually laboris semiotics irure artisan viral hoodie green juice celiac magna. Lumbersexual succulents pickled hashtag mustache. Veniam seitan gastropub, bushwick hexagon hell of vice normcore franzen gluten-free 3 wolf moon slow-carb. Organic lo-fi art party, selvage typewriter et cardigan shabby chic.</p>
-      </section>
-      <section class="content-area">
-        <p>Live-edge sed williamsburg kombucha venmo food truck authentic, wolf DIY. Vape consequat pariatur placeat. Eu assumenda id, vape mixtape semiotics 90's voluptate consequat retro. Organic pork belly four dollar toast waistcoat bespoke 3 wolf moon, cardigan labore hexagon. Et id lyft, meggings iceland scenester vaporware snackwave sustainable succulents iPhone odio. Messenger bag irure quis yuccie, twee paleo duis blog in. Cray waistcoat placeat yr, art party keffiyeh ut live-edge.</p>
-        <p>Vero hashtag mumblecore dolore keffiyeh venmo. Culpa nihil listicle keffiyeh. Quis cillum sed, pitchfork normcore sapiente yr fixie bitters raw denim kale chips four dollar toast culpa. Jianbing mustache irure, quis ad synth roof party man braid non raw denim et banjo pork belly hexagon bushwick. Fashion axe art party poutine thundercats assumenda, humblebrag semiotics vinyl. Affogato poke sustainable deep v sriracha lyft, mixtape laborum truffaut keffiyeh. Poutine fanny pack mixtape, activated charcoal sint austin edison bulb squid disrupt bicycle rights culpa artisan godard hot chicken letterpress.</p>
-        <p>Microdosing narwhal austin fixie do, food truck cardigan nulla farm-to-table actually gochujang. Fanny pack coloring book delectus fap. Umami nesciunt chicharrones proident normcore whatever. Helvetica labore vegan, glossier fanny pack eiusmod keffiyeh copper mug mollit polaroid officia portland nesciunt ad. Narwhal do four dollar toast, XOXO mumblecore laboris ramps. Locavore et lyft laborum, master cleanse tbh nihil irony. Dolor kinfolk mixtape, lomo prism cray elit listicle fingerstache poke.</p>
-        <p>Kogi assumenda excepteur, live-edge echo park try-hard keytar sriracha in reprehenderit consectetur post-ironic. Kombucha everyday carry whatever butcher bushwick, banjo readymade migas. Kitsch paleo pug, heirloom neutra narwhal delectus viral vice twee lo-fi nostrud raclette butcher. Proident vinyl woke tousled, actually laboris semiotics irure artisan viral hoodie green juice celiac magna. Lumbersexual succulents pickled hashtag mustache. Veniam seitan gastropub, bushwick hexagon hell of vice normcore franzen gluten-free 3 wolf moon slow-carb. Organic lo-fi art party, selvage typewriter et cardigan shabby chic.</p>
-      </section>
-      <section class="content-area">
-        <p>Live-edge sed williamsburg kombucha venmo food truck authentic, wolf DIY. Vape consequat pariatur placeat. Eu assumenda id, vape mixtape semiotics 90's voluptate consequat retro. Organic pork belly four dollar toast waistcoat bespoke 3 wolf moon, cardigan labore hexagon. Et id lyft, meggings iceland scenester vaporware snackwave sustainable succulents iPhone odio. Messenger bag irure quis yuccie, twee paleo duis blog in. Cray waistcoat placeat yr, art party keffiyeh ut live-edge.</p>
-        <p>Vero hashtag mumblecore dolore keffiyeh venmo. Culpa nihil listicle keffiyeh. Quis cillum sed, pitchfork normcore sapiente yr fixie bitters raw denim kale chips four dollar toast culpa. Jianbing mustache irure, quis ad synth roof party man braid non raw denim et banjo pork belly hexagon bushwick. Fashion axe art party poutine thundercats assumenda, humblebrag semiotics vinyl. Affogato poke sustainable deep v sriracha lyft, mixtape laborum truffaut keffiyeh. Poutine fanny pack mixtape, activated charcoal sint austin edison bulb squid disrupt bicycle rights culpa artisan godard hot chicken letterpress.</p>
-        <p>Microdosing narwhal austin fixie do, food truck cardigan nulla farm-to-table actually gochujang. Fanny pack coloring book delectus fap. Umami nesciunt chicharrones proident normcore whatever. Helvetica labore vegan, glossier fanny pack eiusmod keffiyeh copper mug mollit polaroid officia portland nesciunt ad. Narwhal do four dollar toast, XOXO mumblecore laboris ramps. Locavore et lyft laborum, master cleanse tbh nihil irony. Dolor kinfolk mixtape, lomo prism cray elit listicle fingerstache poke.</p>
-        <p>Kogi assumenda excepteur, live-edge echo park try-hard keytar sriracha in reprehenderit consectetur post-ironic. Kombucha everyday carry whatever butcher bushwick, banjo readymade migas. Kitsch paleo pug, heirloom neutra narwhal delectus viral vice twee lo-fi nostrud raclette butcher. Proident vinyl woke tousled, actually laboris semiotics irure artisan viral hoodie green juice celiac magna. Lumbersexual succulents pickled hashtag mustache. Veniam seitan gastropub, bushwick hexagon hell of vice normcore franzen gluten-free 3 wolf moon slow-carb. Organic lo-fi art party, selvage typewriter et cardigan shabby chic.</p>
+        <h1>About</h1>
+        <p>I've been doing web development stuff for a good 10/11 years now.</p>
+        <p>Professionally, I work a digital publishers called YUDU Media and been there for years. My Technical skills include solid understanding of HTML, CSS, JavaScript, PHP, and Graphic Design. Or to more specifically break it down... HTML4/5, CSS2/3, Sass, EcmaScript 5 + 6 (Harmony), Angular.js, Typescript, PHP5, MySQL, and mashing up Web APIs like those provided by Google/Twitter etc.</p>
+        <p>To date I've built and submitted iOS/Android/Web apps for small names like the Financial Times, KPMG, Mixmag, the FA to name a few.</p>
+        <p>Currently, I work as an Account Manager advising on the best strategy and way forward for clients. I currently look after Time Out, Screwfix, and just shy of 100 others. What I reeeeaaaally want to do is more freelance development. So hit me up if you've got something you want to create!</p>
       </section>
     </div>
 
@@ -53,7 +120,7 @@
 
   $background: #ffffff;
   $fontColour: #565e75;
-  $masterFont: "Lucida Sans Unicode", "Lucida Grande", sans-serif;
+  $masterFont: 'Cuprum';
 
   html, body {
     margin: 0;
@@ -65,6 +132,7 @@
     background: $background;
     color: $fontColour;
     font-family: $masterFont;
+    font-size: 24px;
   }
 
   h1,h2,h3,h4,h5,h6 {
@@ -72,49 +140,116 @@
     padding: 0;
   }
 
+  h1 {
+    margin-bottom: 64px;
+    font-size: 80px;
+  }
+
   h2 {
+    font-size: 44px;
+  }
+
+  h1, h2 {
     letter-spacing: 4px;
   }
 
-  .background-triangle {
+  .background-triangle-left {
     width: 1200px;
     height: 1200px;
     display: inline-block;
     float: left;
     z-index: 100;
-    background: #eaeaff;
+    background: white;
     position: fixed;
     transform: rotate(-45deg);
-    top: -700px;
+    top: -1000px;
     left: -700px;
   }
+
+    .background-triangle-right {
+      width: 900px;
+      height: 900px;
+      display: inline-block;
+      float: right;
+      z-index: 100;
+      background: white;
+      position: fixed;
+      transform: rotate(-115deg);
+      top: -400px;
+      right: -700px;
+    }
+
+    .nav-item-link:nth-child(even) {
+      background: purple;
+    }
+
+
+  .nav-item-link:nth-child(even) {
+    background: purple;
+  }
+
 
   .big-triangle-wrapping-mask {
     width: 1100px;
     height: 1100px;
     float: left;
-    clip-path: polygon(0% 0%, 100% 0%, 0% 100%);
+    clip-path: polygon(0% 0%, 100% 0%, 0% 80%);
     shape-outside: polygon(0% 0%, 100% 0%, 0% 100%);
     background: url("../assets/img/bg.jpg");
     background-size: cover;
   }
 
+
+  .portfolio-wrapping {
+    width: 700px;
+    height: 700px;
+    z-index: 5;
+    position: absolute;
+  }
+
+  .portfolio-content {
+    z-index: 7;
+    margin-bottom: 512px;
+    padding-right: 256px;
+  }
+
+  .skate {
+    clip-path: polygon(130% 0%, 4% 10%, 39% 80%);
+    shape-outside: polygon(110% 0%, 10% 50%, 90% 70%);
+    background: url("../assets/img/skate.png");
+    background-size: cover;
+    right: 60px;
+  }
+
   .content-area-wrapper {
     float: left;
-    z-index: 1;
+    z-index: 4;
     background: white;
   }
 
 
   .content-area {
     font-size: 2em;
-    position: relative;
     margin-left: 20%;
     margin-right: 30%;
+    margin-bottom: 30%;
   }
 
   .first {
     margin-top: 12%;
+    text-align: right;
+  }
+
+  .portfolio-item {
+    margin-bottom: 512px;
+  }
+
+  p, ul {
+    font-size: 24px;
+  }
+
+  li {
+    font-size: 20px;
   }
 
 
