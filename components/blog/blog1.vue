77b1b36b16fe326e95943aed0015cd79c6dad065
Ben <allobon@gmail.com>
2019-06-18 21:56:22 +0100

    update blog

--- components/blog/blog1.vue
+++ components/blog/blog1.vue
@@ -7,50 +7,45 @@ First, let's create a basic Express app and turn that into a Docker Image.
 
 ### Step 1 - Install docker.
 ### Step 2 - Create express app.
-```
-> npm init -y
-> npm i -S express
+```bash
+npm init -y && npm i -S express
 ```
 ### Step 3 - Open `package.json` and add a start script
-``` javascript
-  "scripts": {
-    "start": "node server.js"
-  }
+```javascript
+"scripts": {
+  "start": "node server.js"
+}
 ```
 ### Step 4 - Open `index.js`
-``` javascript
-  const app = require('express')();
+```javascript
+const app = require('express')();
 
-  app.get('/', (req, res) => { res.send('Awwww yis'); });
+app.get('/', (req, res) => { res.send('Awwww yis'); });
 
-  app.listen(3000, () => console.log('Server running'));
+app.listen(3000, () => console.log('Server running'));
 ```
 ### Step 5 - Create Dockerfile
-``` docker
-  FROM mhart/alpine-node
-
-  WORKDIR /src
-
-  COPY package.json .
-
-  RUN npm i COPY . .
-
-  EXPOSE 3000 CMD ["npm", "start"]
+```docker
+FROM mhart/alpine-node
+WORKDIR /src
+COPY package.json .
+RUN npm i COPY . .
+EXPOSE 3000 CMD ["npm", "start"]
 ```
 _alpine-node is a super tiny linux distro weighing in at 55MB._
 ### Step 6 - Build the image
-```
- > BUILD IMAGE docker build -t IMAGENAME .
+```shell
+BUILD IMAGE docker build -t IMAGENAME .
 ```
 _HINT: to see images type `docker images`_
 ### Step 7 - Run the image
-```
-> RUN IMAGE docker run -p 3000:3000 -d IMAGENAME
+```bash
+RUN IMAGE docker run -p 3000:3000 -d IMAGENAME
 ```
 _HINT: to stop/ kill this type `docker ps` followed by `docker kil APP_NAME`_
 ### Step 8 - Upload the image
-```
- > docker commit -m "building" -a Ben CONTAINER ID vohzd/express`
+```docker
+docker commit -m "building" -a Ben CONTAINER ID vohzd/express
 ```
 ### Step 9 - Congrats!
 
--- components/blog/blog2.vue
+++ components/blog/blog2.vue
@@ -5,58 +5,85 @@ We're going to provision a fresh server, install Nginx, and auto-configure SSL.
 
 I'll assume you have a domain name for this and a choice of VPS provider. There's loads of choices out there like AWS, DigitalOcean, etc, but for this tutorial I will be using Scaleway.
 
-### Step 1 - Log onto Scaleway.
-### Step 1 - SSH into your machine.
-### Step 2 - Create express app.
+### Step 1 - Log onto Scaleway &amp; SSH into your machine
+### Step 2 - Upgrade system
+```bash
+apt update && apt upgrade -y
 ```
-> npm init -y
-> npm i -S express
+### Step 3 - Install Nginx
+```bash
+apt install nginx -y
 ```
-### Step 3 - Open `package.json` and add a start script
-``` javascript
-  "scripts": {
-    "start": "node server.js"
-  }
-```
-### Step 4 - Open `index.js`
-``` javascript
-  const app = require('express')();
+### Step 4 - Create a test html file to serve
 
-  app.get('/', (req, res) => { res.send('Awwww yis'); });
+If you want to host multiple domains on one server, ensure you create subdirectories. Replace example.com with your domain
+```bash
+mkdir -p /var/www/example.com/html
+```
 
-  app.listen(3000, () => console.log('Server running'));
+Let's now edit said file
+```bash
+nano /var/www/example.com/html/index.html
 ```
-### Step 5 - Create Dockerfile
-``` docker
-  FROM mhart/alpine-node
 
-  WORKDIR /src
+Add in some basic HTML to start
+```html
+<html>
+    <head>
+        <title>Welcome to example.com</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+        <p>You have accessed the example.com website.</p>
+    </body>
+</html>
+```
+### Step 6 - Create a server to distribute files
+```bash
+nano /etc/nginx/sites-available/example.com
+```
 
-  COPY package.json .
+Within this, add in the following
+```nginx
+server {
+        listen 80;
+        listen [::]:80;
 
-  RUN npm i COPY . .
+        root /var/www/example.com/html;
+        index index.html index.htm;
 
-  EXPOSE 3000 CMD ["npm", "start"]
-```
-_alpine-node is a super tiny linux distro weighing in at 55MB._
-### Step 6 - Build the image
-```
- > BUILD IMAGE docker build -t IMAGENAME .
-```
-_HINT: to see images type `docker images`_
-### Step 7 - Run the image
-```
-> RUN IMAGE docker run -p 3000:3000 -d IMAGENAME
-```
-_HINT: to stop/ kill this type `docker ps` followed by `docker kil APP_NAME`_
-### Step 8 - Upload the image
-```
- > docker commit -m "building" -a Ben CONTAINER ID vohzd/express`
-```
-### Step 9 - Congrats!
+        server_name example.com www.example.com;
 
-_HINT: to kill EVERYTHING type `docker system prune -a` (the `-a` flag goes nuclear)_
-
+        location / {
+                try_files $uri $uri/ =404;
+        }
+}
+```
+Important: Edit the lines root and server_name according to your domain name.
+### Step 7 - Symbolic link sites-available to sites-enabled
+```
+ln -s /etc/nginx/sites-available/example.com /etc/nginx/sites-enabled/
+```
+### Step 8 - Verify and reboot server
+```
+nginx -t
+systemctl restart nginx
+```
+You should now see your server over http
+### Step 9 - Install SSL using Certbot
+Get the latest package repo
+```
+add-apt-repository ppa:certbot/certbot
+```
+Install certbot for nginx
+```
+apt install python-certbot-nginx -y
+```
+Launch the wizard
+```
+certbot --nginx -d example.com -d www.example.com
+```
+Done!
 </template>
 
 <script>
