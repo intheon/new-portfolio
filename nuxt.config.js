f1137a78aeb2d5a958a81a9602b7ae05994caff2
Ben <allobon@gmail.com>
2020-01-05 17:03:52 +0000

    modernise with SSR build step + caprover

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,46 +1,44 @@
-export const head = {
-  title: "Ben Smith - Full Stack Developer",
-  meta: [
-    { charset: "utf-8" },
-    { name: "Author", content: "Ben Smith AKA vohzd" },
-    { name: "viewport", content: "width=device-width, initial-scale=1" },
-    {
-      name: "google-site-verification",
-      content: "YFLjJEzwNtdNatFp0w4SBmfHPtVMfcWyD9Pp1eJxzW4"
-    },
-    {
-      name: "keywords",
-      content:
-        "javascript, developer, vue, devops, nuxt, freelance, tutorial, ecommerce, conversion, rate, optimisation"
-    },
-    {
-      hid: "description",
-      name: "description",
-      content: "Ben Smith AKA vohzd | Marvellous full-stack developer!"
-    }
+module.exports = {
+  title: "Vohzd | Full Stack JS Developer",
+  head: {
+    meta: [
+      { charset: "utf-8" },
+      { name: "Author", content: "vohzd" },
+      { name: "google-site-verification", content: "YFLjJEzwNtdNatFp0w4SBmfHPtVMfcWyD9Pp1eJxzW4" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { name: "keywords", content: "javascript, developer, vue, devops, nuxt, freelance, tutorial, ecommerce, conversion, rate, optimisation" },
+      { hid: "description", name: "description", content: "vohzd | Marvellous full-stack developer!" }
+    ],
+    link: [
+      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+      { rel: "canonical", href: "https://vohzd.com" },
+      { rel: "stylesheet", href: "https://fonts.googleapis.com/css?family=Saira+Semi+Condensed|Stylish" }
+    ],
+    script: [{ src: "http://localhost:1337/libraries/main.js" }]
+  },
+  loading: { color: "#552a3f" },
+  router: {
+    base: "/"
+  },
+  modules: [
+    "@nuxtjs/axios",
+    "@nuxtjs/markdownit",
+    "@nuxtjs/sitemap",
+    [ "@nuxtjs/google-analytics", { id: "UA-104309082-3" } ]
   ],
-  link: [
-    { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
-    { rel: "canonical", href: "https://vohzd.com" },
-    {
-      rel: "stylesheet",
-      href:
-        "https://fonts.googleapis.com/css?family=Saira+Semi+Condensed|Stylish"
-    }
-  ],
-  script: [{ src: "http://localhost:1337/libraries/main.js" }]
-};
-export const plugins = [
-  "~/plugins/date.js"
-];
-export const modules = [
-  ["@nuxtjs/google-analytics", { id: "UA-104309082-3" }],
-  "@nuxtjs/markdownit",
-  "@nuxtjs/sitemap",
-  "@nuxtjs/axios"
-];
-export const sitemap = {
-  hostname: "https://vohzd.com",
-  gzip: true
-};
-export const loading = { color: "#552a3f" };
+  build: {
+    optimizeCSS: true
+  },
+  sitemap: {
+    hostname: "https://vohzd.com",
+    gzip: true
+  },
+  axios: {
+    baseURL: "/",
+    proxyHeaders: false,
+    credentials: true
+  },
+  plugins: [
+    "~/plugins/date.js",
+  ]
+}
