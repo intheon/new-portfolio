748cc3900d6734e4cc92dde92adc767b6e9d3d81
Ben <allobon@gmail.com>
2020-11-24 19:51:31 +0000

    add email

--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -14,6 +14,9 @@
         <div class="c20 right">
           <a href="https://github.com/vohzd" target="_blank" class="text-center"><font-awesome-icon :icon="['fab', 'github']"></font-awesome-icon> @vohzd</a>
         </div>
+        <div class="row">
+          Email me: <a href="mailto:website@vohzd.com">website@vohzd.com</a>
+        </div>
       </div>
     </div>
   </div>
